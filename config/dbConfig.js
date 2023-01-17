fdf612803dae15d6e894f8fbae2e17829a0d8c25
M Vinicios Nativo <onativo@outlook.com>
2023-01-17 19:55:27 -0300

    'empty'

--- config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-if(process.env.NODE_ENV = 'blogApp-prod'){
-  module.exports = {
-    mongoURI: "mongodb+srv://vini:cr7NadKub8zaHJvt@blogapp-prod.opnieg2.mongodb.net/test"
-  }
-}else{
-  module.exports = {
-    mongoURI: 'mongodb://127.0.0.1:27017/blogApp'
-  }
-}
