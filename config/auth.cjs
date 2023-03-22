ca632793172c6d8c3fc08c77e13e42efabe730aa
M Vinicios Nativo <onativo@outlook.com>
2023-03-22 20:55:29 -0300

    added auth and config files

---
+++ config/auth.cjs
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose')
+const localStrategy = require('passport-local').Strategy
+
+
+require ('../models/User.js')
+const User = mongoose.model('users')
+
+module.exports =  function(passport){
+  passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, (email, password, done) => {
+    User.findOne({email: email})
+      .then((user) => {
+        if(!user){
+          return done(null, false, {message: 'Esta conta não existe. Cadastre-se primeiro :)'})
+        }
+        bcrypt.compare(password, user.password, (err, match) => {
+          if(match){
+            return done(null, user)
+          }else{
+            return done(null, false, {message: 'Senha incorreta. Tente recuperá-la.'})
+          }
+        })
+      })
+    }
+  ))
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user)
+    })
+  })
+}
